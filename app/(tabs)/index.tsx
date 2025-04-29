e07ad44ae0c5d81fb03d1cea028d80079f019106
Diogo Alvino <contato.diogodev7@gmail.com>
2025-04-29 20:50:39 -0300

    feat: adiciona tela de cadastro de equipamento

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,8 +1,9 @@
 import { Stack } from 'expo-router';
 import { StyleSheet, View } from 'react-native';
 import Header from '~/components/layout/header';
-
 import { ScreenContent } from '~/components/ScreenContent';
+
+import { EquipmentRegistration } from '~/views/equipmentRegistration';
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <Header title="Cadastro de equipamento" />
 
       <View style={styles.container}>
+        {/*<EquipmentRegistration path="app/(tabs)/index.tsx" title="Tab One" />*/}
         <ScreenContent path="app/(tabs)/index.tsx" title="Tab One" />
       </View>
     </>
---
+++ views/equipmentRegistration.tsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import { ScrollView, StyleSheet, View } from 'react-native';
+
+import MainButton from '~/components/buttons/mainButton';
+import PrimaryInput from '~/components/inputs/primaryInput';
+import PrimarySelect from '~/components/inputs/primarySelect';
+import PrimarySection from '~/components/sections/primarySection';
+
+type EquipmentRegistrationProps = {
+  title: string;
+  path: string;
+  children?: React.ReactNode;
+};
+
+export const EquipmentRegistration = ({}: EquipmentRegistrationProps) => {
+  const [modelo, setModelo] = useState<string | undefined>(undefined);
+
+  return (
+    <ScrollView contentContainerStyle={styles.container} style={{ flex: 1 }}>
+      <PrimarySection title="Dados Iniciais">
+        <PrimaryInput label="Edificação" placeholder="Informe" />
+        <PrimaryInput label="Endereço" placeholder="Informe" />
+        <PrimaryInput label="Responsável" placeholder="Informe" />
+        <PrimaryInput label="Função" placeholder="Informe" />
+        <PrimaryInput label="Telefone" placeholder="Informe" />
+        <PrimaryInput label="E-mail" placeholder="Informe" />
+      </PrimarySection>
+
+      <PrimarySection title="Informações Gerais do Equipamento">
+        <PrimaryInput label="Data da Instalação" placeholder="Informe" />
+        <PrimaryInput label="Identificação do Equipamento" placeholder="Informe" />
+        <PrimaryInput label="Fabricante" placeholder="Informe" />
+        <PrimaryInput label="CNPJ" placeholder="Informe" />
+        <PrimarySelect
+          label="Modelo"
+          placeholder="Selecione o item"
+          options={['Modelo A', 'Modelo B', 'Modelo C']}
+          selected={modelo}
+          onSelect={(value) => setModelo(value)}
+        />
+        <PrimaryInput label="Empresa Conservadora" placeholder="Informe" />
+        <PrimaryInput label="CNPJ" placeholder="Informe" />
+        <PrimaryInput label="Capacidade Nominal (kg)" placeholder="Informe" />
+        <PrimaryInput label="Lotacão" placeholder="Informe" />
+        <PrimaryInput label="Velocidade nominal (m/s)" placeholder="Informe" />
+        <PrimaryInput label="Tipo de Uso (Residencial, Comercial, Público)" placeholder="Informe" />
+        <PrimaryInput label="Número de paradas" placeholder="Informe" />
+        <PrimaryInput label="Casa de Máquinas (Existente, Não Existente)" placeholder="Informe" />
+      </PrimarySection>
+
+      <MainButton title="Cadastrar" type="primary" />
+      <MainButton title="Cancelar" type="secondary" />
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 20,
+  },
+});
